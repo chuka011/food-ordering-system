0c316797ff09f6548811d72db270df1cb9bff7fb
BranislavCukanovic <chuka011@gmail.com>
2023-02-07 13:00:18 +0100

    Fix totalOrderPrice validation

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
@@ -79,7 +79,7 @@ public class Order extends AggregateRoot<OrderId> {
     }
 
     private void validateTotalPrice() {
-        if(price == null || price.isGreaterThanZero()) {
+        if(price == null || !price.isGreaterThanZero()) {
             throw new OrderDomainException("Total price must be grater than zero!");
         }
     }
