72f2f7af5a2368878294592eaa005cdef8bb34cc
BranislavCukanovic <chuka011@gmail.com>
2023-02-09 13:53:18 +0100

    Adding JPA repository and adapter implementation for customer and restaurant

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/entity/RestaurantEntityId.java
@@ -0,0 +1,35 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.entity;
+
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(RestaurantEntityId.class)
+public class RestaurantEntityId implements Serializable {
+
+    private UUID restaurantId;
+    private UUID productId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RestaurantEntityId that = (RestaurantEntityId) o;
+        return restaurantId.equals(that.restaurantId) && productId.equals(that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(restaurantId, productId);
+    }
+}
